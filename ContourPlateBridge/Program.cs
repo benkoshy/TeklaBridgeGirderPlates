3c7004301a746b9b704e5d64d122d442b949809a
BKSpurgeon <benkoshy@hotmail.com>
2020-08-06 14:25:10 +1000

    Add: parameterize the name of the countour plate

--- ContourPlateBridge/Program.cs
+++ ContourPlateBridge/Program.cs
@@ -20,20 +20,21 @@ namespace ContourPlateBridge
                 // Put 20 plates in 1 row. Use a space of 300 between plates.
                 // Name into the name field
 
-                double a = 470;
-                double b = 570;
-                
+                double aWidth = 470;
+                double bLength = 570;
+                string name = "B81-P09-BER-01";
+
                     // we're going counter clock wise
                 ContourPoint p1 = new ContourPoint(new Point(0, 0, 0), new Chamfer(0,0, Chamfer.ChamferTypeEnum.CHAMFER_LINE));
                 p1.Chamfer.DZ1 = -12;
                 
-                ContourPoint p2 = new ContourPoint(new Point(a, 0, 0), new Chamfer(0, 0, Chamfer.ChamferTypeEnum.CHAMFER_LINE));
+                ContourPoint p2 = new ContourPoint(new Point(aWidth, 0, 0), new Chamfer(0, 0, Chamfer.ChamferTypeEnum.CHAMFER_LINE));
                 p2.Chamfer.DZ1 = -6;
 
-                ContourPoint p3 = new ContourPoint(new Point(a, b, 0), new Chamfer(0, 0, Chamfer.ChamferTypeEnum.CHAMFER_LINE));
+                ContourPoint p3 = new ContourPoint(new Point(aWidth, bLength, 0), new Chamfer(0, 0, Chamfer.ChamferTypeEnum.CHAMFER_LINE));
                 p3.Chamfer.DZ1 = 0;
 
-                ContourPoint p4 = new ContourPoint(new Point(0, b, 0), new Chamfer(0, 0, Chamfer.ChamferTypeEnum.CHAMFER_LINE));
+                ContourPoint p4 = new ContourPoint(new Point(0, bLength, 0), new Chamfer(0, 0, Chamfer.ChamferTypeEnum.CHAMFER_LINE));
                 p4.Chamfer.DZ1 = -6;
                 
                 ContourPlate contourPlate = new ContourPlate();
@@ -45,6 +46,7 @@ namespace ContourPlateBridge
                 contourPlate.Finish = "FOO";
                 contourPlate.Profile.ProfileString = "PL32";
                 contourPlate.Material.MaterialString = "250";
+                contourPlate.Name = name;
                 contourPlate.Position.Depth = Position.DepthEnum.FRONT;
 
                 contourPlate.Insert();
