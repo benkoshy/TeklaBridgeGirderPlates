9f9e910c08cbaaffea64a676b3413d9cd5c24444
BKSpurgeon <benkoshy@hotmail.com>
2020-08-07 17:04:12 +1000

    Add: successfully read data from the CSV file

---
+++ ContourPlateBridge/PlateData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CsvHelper;
+using CsvHelper.Configuration.Attributes;
+
+namespace ContourPlateBridge
+{
+    class PlateData
+    {
+        [Name("BEARING MARK")]
+        public string BearingMark { get; set; }
+
+        [Name("DIM A")]
+        public int DimA { get; set; }
+
+        [Name("DIM B")]
+        public int DimB { get; set; }
+
+        [Name("T1")]
+        public int T1 { get; set; }
+
+        [Name("T2")]
+        public int T2 { get; set; }
+
+        [Name("T3")]
+        public int T3 { get; set; }
+
+        [Name("T4")]
+        public int T4 { get; set; }
+
+        [Name("PROFILE")]
+        public int Profile { get; set; }
+    }
+}
--- ContourPlateBridge/Program.cs
+++ ContourPlateBridge/Program.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using Tekla.Structures.Geometry3d;
 using Tekla.Structures.Model;
 
+using CsvHelper;
+using System.IO;
+
+
 namespace ContourPlateBridge
 {
     class Program
@@ -16,8 +20,17 @@ namespace ContourPlateBridge
 
             if (model.GetConnectionStatus())
             {
-                SmartContourPlate contourPlate = new SmartContourPlate(model, 0,0);
-                contourPlate.addContourPlate();
+                using (var reader = new StreamReader(@"C:\Users\Koshy\source\repos\ContourPlateBridge\COMBINED B81-AND-B80- Bearing Schedules.csv"))
+                using (var csv = new CsvReader(reader, System.Globalization.CultureInfo.InvariantCulture))
+                {                    
+                    var plates = csv.GetRecords<PlateData>();                   
+
+                    foreach (PlateData plate in plates)
+                    {
+                        SmartContourPlate contourPlate = new SmartContourPlate(model, 0, 0, plate.Profile, plate.T1, plate.T2, plate.T3, plate.T4);
+                        contourPlate.addContourPlate();
+                    }
+                }
             }
 
             model.CommitChanges();
