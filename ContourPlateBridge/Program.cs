0cc7907ba4ace27e88a90f8aa438b516d12d59a6
BKSpurgeon <benkoshy@hotmail.com>
2020-08-07 18:18:22 +1000

    Add: tiling of all the countour plates

--- ContourPlateBridge/Program.cs
+++ ContourPlateBridge/Program.cs
@@ -23,17 +23,39 @@ namespace ContourPlateBridge
                 using (var reader = new StreamReader(@"C:\Users\Koshy\source\repos\ContourPlateBridge\COMBINED B81-AND-B80- Bearing Schedules.csv"))
                 using (var csv = new CsvReader(reader, System.Globalization.CultureInfo.InvariantCulture))
                 {                    
-                    var plates = csv.GetRecords<PlateData>();                   
+                    var plates = csv.GetRecords<PlateData>();
+
+                    int rowCount = 0;
+                    int columnCount = 0;
+
+                    //PlateData plate = plates.First();
+                    //SmartContourPlate contourPlate = new SmartContourPlate(model, 0, 0, plate.Profile, plate.T1, plate.T2, plate.T3, plate.T4, plate.DimA, plate.DimB, plate.BearingMark);
+                    //contourPlate.addContourPlate();
 
                     foreach (PlateData plate in plates)
                     {
-                        SmartContourPlate contourPlate = new SmartContourPlate(model, 0, 0, plate.Profile, plate.T1, plate.T2, plate.T3, plate.T4, plate.DimA, plate.DimB, plate.BearingMark);
+                        int xInsertionPoint = rowCount * 500;
+                        int yInsertionPoint = columnCount * 720;
+
+                        if (rowCount == 20)
+                        {
+                            columnCount++;
+                            rowCount = 0;
+                        }
+                        else
+                        {
+                            rowCount++;
+                        }                        
+
+                        SmartContourPlate contourPlate = new SmartContourPlate(model, xInsertionPoint, yInsertionPoint, plate.Profile, plate.T1, plate.T2, plate.T3, plate.T4, plate.DimA, plate.DimB, plate.BearingMark);
                         contourPlate.addContourPlate();
                     }
                 }
             }
 
             model.CommitChanges();
+
+            Console.ReadLine();
         }
 
         
